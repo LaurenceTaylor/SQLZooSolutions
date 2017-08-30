d9e17182a31be0f4112fd332005cb1cd6d0a0f71
Laurence <ltaylorapp@gmail.com>
2017-08-30 20:56:49 +0100

    Update SELECT-basics.sql
    Neaten code

--- SELECT-basics.sql
+++ SELECT-basics.sql
@@ -3,7 +3,8 @@
 -- Note that strings (pieces of text that are data) should be in 'single quotes';
 -- Modify it to show the population of Germany
 
-SELECT population FROM world
+SELECT population 
+FROM world
 WHERE name = 'Germany';
 
 -- #2 Scandinavia
@@ -11,7 +12,8 @@ WHERE name = 'Germany';
 -- The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
 -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 
-SELECT name, population FROM world
+SELECT name, population 
+FROM world
 WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
 -- #3 Just the right size
@@ -20,6 +22,7 @@ WHERE name IN ('Sweden', 'Norway', 'Denmark');
 -- The example below shows countries with an area of 250,000-300,000 sq. km. 
 -- Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
 
-SELECT name, area FROM world
+SELECT name, area 
+FROM world
 WHERE area BETWEEN 200000 AND 250000;
 
