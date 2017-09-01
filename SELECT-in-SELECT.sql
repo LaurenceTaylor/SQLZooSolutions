130e8e85821e1a955df7212544773fd43d8557f0
Laurence <ltaylorapp@gmail.com>
2017-09-01 12:29:38 +0100

    Update SELECT-in-SELECT.sql

--- SELECT-in-SELECT.sql
+++ SELECT-in-SELECT.sql
@@ -18,3 +18,66 @@ WHERE gdp/population >
        FROM world
        WHERE name = 'United Kingdom')
 AND continent = 'Europe';
+
+-- #3 Neighbours of Argentina and Australia
+-- List the name and continent of countries in the continents containing either Argentina or Australia.
+-- Order by name of the country.
+
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent
+                    FROM world
+                    WHERE name = 'Argentina'
+                    OR name = 'Australia')
+ORDER BY name;
+
+-- #4 Between Canada and Poland
+-- Which country has a population that is more than Canada but less than Poland?
+-- Show the name and the population.
+
+SELECT name, population
+FROM world
+WHERE population >
+      (SELECT population
+       FROM world
+       WHERE name = 'Canada')
+AND population <
+    (SELECT population
+     FROM world
+     WHERE name = 'Poland');
+     
+-- #5 Percentages of Germany
+-- Germany (population 80 million) has the largest population of the countries in Europe.
+-- Austria (population 8.5 million) has 11% of the population of Germany.
+-- Show the name and the population of each country in Europe.
+-- Show the population as a percentage of the population of Germany.
+
+SELECT name, CONCAT(ROUND(population/
+                         (SELECT population
+                          FROM world
+                          WHERE name = 'Germany') * 100, 0), '%')
+FROM world
+WHERE continent = 'Europe';
+
+-- #6 Bigger than every country in Europe
+-- Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+
+SELECT name
+FROM world
+WHERE gdp > ALL(SELECT gdp
+                FROM world
+                WHERE continent = 'Europe'
+                AND gdp > 0);
+                
+-- #7 Largest in each continent
+-- Find the largest country (by area) in each continent, show the continent, the name and the area:
+
+SELECT continent, name, area
+FROM world x
+WHERE area >= ALL(SELECT area 
+                  FROM world y
+                  WHERE y.continent = x.continent
+                  AND area > 0);
+
+-- #8 First country of each continent (alphabetically)
+-- List each continent and the name of the country that comes first alphabetically.
