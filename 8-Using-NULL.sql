188ae976a009206d56c10cc2192989a3a38680ac
Laurence <ltaylorapp@gmail.com>
2017-09-03 22:37:03 +0100

    Update 8-Using-NULL.sql

--- 8-Using-NULL.sql
+++ 8-Using-NULL.sql
@@ -1,1 +1,24 @@
+-- #1 NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+-- List the teachers who have NULL for their department.
 
+SELECT name
+FROM teacher
+WHERE dept IS NULL;
+
+-- #2
+-- Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+
+SELECT teacher.name, dept.name
+FROM teacher INNER JOIN dept ON (teacher.dept = dept.id);
+
+-- #3
+-- Use a different JOIN so that all teachers are listed.
+
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+-- #4
+-- Use a different JOIN so that all departments are listed.
+
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id);
