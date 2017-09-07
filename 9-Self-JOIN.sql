d5bbc9d443f901276e15d110b09bf3ba6fa26d59
Laurence <ltaylorapp@gmail.com>
2017-09-07 20:56:58 +0100

    Update 9-Self-JOIN.sql

--- 9-Self-JOIN.sql
+++ 9-Self-JOIN.sql
@@ -57,5 +57,29 @@ AND stopb.name = 'London Road';
 -- #7 Using a self join
 -- Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
 
+SELECT DISTINCT a.company, a.num
+FROM route AS a 
+JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+WHERE a.stop = 115
+AND b.stop = 137;
+
+-- #8
+-- Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+
+SELECT a.company, a.num
+FROM route AS a
+JOIN route AS b ON (a.num = b.num AND a.company = b.company)
+JOIN stops AS stopa ON (a.stop = stopa.id)
+JOIN stops AS stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart'
+AND stopb.name = 'Tollcross';
+
+-- #9
+/* Give a distinct list of the stops which may be reached from 'Craiglockhart' 
+by taking one bus, including 'Craiglockhart' itself, offered by the LRT company.
+Include the company and bus no. of the relevant services.*/
+
+
+
 
 
